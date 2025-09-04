32eb5ab2d52a52e37549e6fbac4917798e7c8ceb
vinaylalco <49960570+vinaylalco@users.noreply.github.com>
2025-09-04 11:13:02 +0800

    feat: add Indonesian translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,13 +2,14 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
 import es from "./locales/es.json";
+import id from "./locales/id.json";
 
 // Detect browser language
 let browserLang = navigator.language.split("-")[0];
-let lng = browserLang === "es" ? "es" : "en";
+let lng = browserLang === "es" ? "es" : browserLang === "id" ? "id" : "en";
 
 i18n.use(initReactI18next).init({
-  resources: { en: { translation: en }, es: { translation: es } },
+  resources: { en: { translation: en }, es: { translation: es }, id: { translation: id } },
   lng,
   fallbackLng: "en",
   interpolation: { escapeValue: false }
