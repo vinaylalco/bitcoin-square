98d6b7b3612810abe30007aba2018362fec1a4f0
Vinay Lal <onethruten@gmail.com>
2025-09-30 10:01:21 +0800

    fixing youtube bug

--- src/components/lesson/YouTubeVideo.tsx
+++ src/components/lesson/YouTubeVideo.tsx
@@ -266,9 +266,9 @@ export default function YouTubeVideo({
               </svg>
               <span>Loading video…</span>
             </div>
-          )}
+          </div>
         </div>
       </div>
-    </div>
+    </div> 
   );
 }
