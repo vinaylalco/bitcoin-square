bc5d5761f2d9acbbdea80b0e0bf6948a31981022
vinaylalco <49960570+vinaylalco@users.noreply.github.com>
2025-10-06 10:18:57 +0800

    Integrate Stripe Checkout for courses

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_STRAPI_URL?: string;
+  readonly VITE_STRIPE_PUBLIC_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
